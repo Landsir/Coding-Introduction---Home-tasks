114fa9d2af956594221328ebdd906406b59c2607
Anna_Maslova <personnel2003@yandex.ru>
2022-08-14 10:46:57 +0300

    Task004

---
+++ task004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число, больше 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+    while (number1 >= count)
+    {
+        Console.WriteLine(count);
+        count = count + 2;
+    }
+
+
