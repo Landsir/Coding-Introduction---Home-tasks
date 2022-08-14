43426a57518bf83111424e2ead6ce02565144bb0
Anna_Maslova <personnel2003@yandex.ru>
2022-08-14 10:02:15 +0300

    Task003

---
+++ task003/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число, чтобы узнать, является ли число четным.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0)
+    Console.WriteLine("Ваше число четное");
+    else 
+    Console.WriteLine("Ваше число нечетное");
