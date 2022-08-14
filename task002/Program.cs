0b9469bbe597b96b09871555f2260c7253172943
Anna_Maslova <personnel2003@yandex.ru>
2022-08-14 09:47:54 +0300

    Task002

---
+++ task002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите три числа, чтобы узнать, какое большее.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if (number2>max)
+max = number2;
+    if (number3>max)
+    max = number3;
+    
+Console.WriteLine("Большее число: " + max);
