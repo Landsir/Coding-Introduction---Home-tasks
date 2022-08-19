8784b5f4a66415498397a8c87ba8e19ecc18df73
Anna_Maslova <personnel2003@yandex.ru>
2022-08-19 18:28:58 +0300

    Task010

---
+++ task010/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите трехзначное число");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < 999)
+        {
+            if (number > 99)
+            {
+                number = number % 100;
+                number = number / 10;
+
+                Console.WriteLine(number);
+
+            }
+            else Console.WriteLine("Вы ввели неверное число");   
+        }
+
+        else Console.WriteLine("Вы ввели неверное число");
+    }
+}
