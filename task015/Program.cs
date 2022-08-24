a9ca22e471b2f73599cf24ea6219c7142ad067eb
Anna_Maslova <personnel2003@yandex.ru>
2022-08-24 07:48:22 +0300

    task 015

---
+++ task015/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+Console.WriteLine("Введите день недели числом");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day>0 && day < 7)
+    {
+        if(day==7 || day==6)
+        {
+            Console.WriteLine(day + "-ой день недели - выходной день.");
+        }
+        else Console.Write(day + " - это рабочий день.");
+    } 
+else Console.WriteLine(" Это число не обозначает день недели.");
+}
+}
