119ed522c4dfaa347a05ef89e23d0594545d2fb4
Anna_Maslova <personnel2003@yandex.ru>
2022-09-03 07:41:11 +0300

    Task 025

---
+++ task025/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Input number A ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int result = numberA;
+
+while (count < numberB-1)
+{
+result = (result * numberA);
+count++;
+}
+Console.WriteLine(result);
+
