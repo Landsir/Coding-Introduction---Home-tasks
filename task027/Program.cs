e9497dd245d3fa30d4e1f9d88db647c0a96d412c
Anna_Maslova <personnel2003@yandex.ru>
2022-09-03 08:13:55 +0300

    Task 027

---
+++ task027/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input number");
+string number = Console.ReadLine();
+int result = 0;
+
+for (int i = 0; i < number.Length; i++)
+{
+    result += number[i] - '0';
+}
+Console.WriteLine(result);
